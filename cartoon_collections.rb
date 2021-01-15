10db1d849f5d4b20e21cfec35265d73768027092
prodigysurge <boatinglife6@yahoo.com>
2021-01-14 19:46:34 -0500

    commit tree ca0e11cc78c5772d826bfd00e7e3555e872e7dce

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+    puts "#{index + 1} #{name}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+
+def summon_captain_planet(arr)
+  arr.map do |name| 
+    name.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+
+def long_planeteer_calls(arr)
+  value = arr.find { |i| i.length > 4}
+  value == nil ? false : true
+end
+   
+
+      
+
+
+
+
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
+
